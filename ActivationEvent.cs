6d5283074ca84368fef1f2a8ab8e7aa643005022
sylvainpasquier <43677337+sylvainpasquier@users.noreply.github.com>
2019-11-05 18:05:58 +0100

    Initial commit

---
+++ ActivationEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationEvent : MonoBehaviour
+{
+    Animation anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        if (!anim.isPlaying && Input.GetKey(KeyCode.P))
+        {
+            anim.Play("movement");
+        }
+    }
+}
